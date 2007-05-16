e27ca4212aa2a27c2eef1e08ab8441c7fdff4541
jfrench2 <jfrench2@22698fe0-d021-0410-a9a5-dc0f9d26e629>
2007-05-16 22:24:44 +0000

    Proper Invite model and Unit Test
    git-svn-id: http://vitamin-j.com/svn/apps/mix/trunk@173 22698fe0-d021-0410-a9a5-dc0f9d26e629

---
+++ app/models/invite.rb
@@ -0,0 +1,38 @@
+gem 'uuidtools'
+require 'uuidtools'
+
+class Invite < ActiveRecord::Base
+  belongs_to  :user,
+              :foreign_key => 'from'
+              
+  validates_presence_of :to_email
+  validates_format_of :to_email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates_presence_of :from_user
+  
+  def from=(from)
+    self.from_user = from.id
+  end
+  
+  def to=(to)
+    self.to_email = to
+  end
+  
+  def accept
+    if 'open' == self.status
+      self.status = 'accepted'
+      self.accepted_at = Time.now
+      save
+    else
+      throw :already_accepted
+    end
+  end
+  
+  def before_create
+    self.uuid = UUID.random_create.to_s.gsub(/\W/, '').upcase
+    self.status = 'open'
+  end
+  
+  def validate
+    errors.add(:to_email, "#{to_email} is already a member") if User.count(:conditions => {:email => to_email}) > 0
+  end
+end
---
+++ test/unit/invite_test.rb
@@ -0,0 +1,62 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class InviteTest < Test::Unit::TestCase
+  fixtures :invites, :users
+
+  def setup
+    @aaron = users(:aaron)
+    @quentin = users(:quentin)
+  end
+  
+  def test_should_reject_bad_email
+    assert_no_difference(Invite, :count) do
+      Invite.create(:from => @quentin, :to => 'not an email')
+      Invite.create(:from => @quentin, :to => 'email@')
+      Invite.create(:from => @quentin, :to => '@email.com')
+      Invite.create(:from => @quentin, :to => nil)
+    end
+  end
+  
+  def test_should_create
+    assert_difference(Invite, :count, 1) do
+      i = Invite.create(:from => @quentin, :to => 'josh@vitamin-j.com')
+      assert_equal 32, i.uuid.length
+      assert_equal 'open', i.status
+    end
+  end
+  
+  def test_should_not_invite_existing_user
+    assert_no_difference(Invite, :count) do
+      i = Invite.create(:from => @aaron, :to => @quentin.email)
+      assert i.errors.on :to_email
+    end
+  end
+  
+  def test_should_not_invite_bad_email
+    assert_no_difference(Invite, :count) do
+      i = Invite.create(:from => users(:aaron), :to => 'test@foo')
+      assert !i.valid?
+      assert i.errors.on :to_email
+      
+      i = Invite.create(:from => users(:aaron), :to => nil)
+      assert !i.valid?
+      assert i.errors.on :to_email
+    end
+  end
+  
+  def test_should_accept_open_invite
+    i = invites(:open)
+    i.accept
+    i.reload
+    assert_equal 'accepted', i.status
+    assert_not_nil i.accepted_at
+  end
+  
+  def test_should_not_accept_invite_twice
+    i = invites(:accepted)
+    assert_throws :already_accepted do
+      i.accept
+    end
+  end
+
+end
