90cda04f216600e50836b20294d076c82c0917be
jfrench2 <jfrench2@22698fe0-d021-0410-a9a5-dc0f9d26e629>
2007-05-22 21:23:15 +0000

    Added last_invite partial
    git-svn-id: http://vitamin-j.com/svn/apps/mix/trunk@186 22698fe0-d021-0410-a9a5-dc0f9d26e629

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -2,4 +2,8 @@ module InvitesHelper
   def do_invite_form_for(user)
     render :partial => user.invite_count > 0 ? "form" : "no_invites"
   end
+  
+  def do_form_with_last_invite_message(user)
+    render :partial => user.invite_count > 0 ? "form" : "last_invite"
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,16 @@
-star_on = new Image;
-star_off = new Image;
-mail_on = new Image;
-mail_off = new Image;
+var star_on = new Image;
+var star_off = new Image;
+var mail_on = new Image;
+var mail_off = new Image;
 
 star_on.src = "/images/star_on.png";
 star_off.src = "/images/star_off.png";
 mail_on.src = "/images/mail_on.png";
 mail_off.src = "/images/mail_off.png";
+
+window.onload = function () {
+	accordion = new fx.Accordion(document.getElementsByClassName('tog'), 
+								 document.getElementsByClassName('el'), 
+								{ opacity: false,
+								  duration: 350 } );
+}
