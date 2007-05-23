0c87d3131ec98ab96c2c0d0608f09636a782469d
jfrench2 <jfrench2@22698fe0-d021-0410-a9a5-dc0f9d26e629>
2007-05-23 00:43:28 +0000

    Added site controller
    git-svn-id: http://vitamin-j.com/svn/apps/mix/trunk@188 22698fe0-d021-0410-a9a5-dc0f9d26e629

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,3 @@
+class SiteController < ApplicationController
+  layout false
+end
--- app/helpers/swapsets_helper.rb
+++ app/helpers/swapsets_helper.rb
@@ -6,4 +6,8 @@ module SwapsetsHelper
   def other_users_in(set)
     set.users - [current_user]
   end
+  
+  def mini_star_for(user, set)
+    render :partial => "mini_star" if current_user.favorited(user, set)
+  end
 end
