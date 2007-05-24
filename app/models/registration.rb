300eadb62cdee760eef0754196f3d3ef963efc6b
jfrench2 <jfrench2@22698fe0-d021-0410-a9a5-dc0f9d26e629>
2007-05-24 19:49:45 +0000

    Removed superfluous validations on assignment model
    git-svn-id: http://vitamin-j.com/svn/apps/mix/trunk@200 22698fe0-d021-0410-a9a5-dc0f9d26e629

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -8,11 +8,4 @@ class Registration < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :swap_id
   
-  protected
-  def validate
-    errors.add_to_base "Duplicate registration" if self.class.count(:conditions => { :swap_id => swap_id, :user_id => user_id }) > 0
-    errors.add :swap, "Invalid Swap ID"  if Swap.count(:conditions => { :id => swap_id }) < 1
-    errors.add :user, "Did not complete last swap" unless User.find_by_id(user_id).ok_to_play?
-  end
-  
 end
