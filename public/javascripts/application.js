8428018f193deaaaba2c7128c03277ad3c622d69
jfrench2 <jfrench2@22698fe0-d021-0410-a9a5-dc0f9d26e629>
2007-06-24 21:09:39 +0000

    Got AJAX updates to clear out flash divs on invites
    git-svn-id: http://vitamin-j.com/svn/apps/mix/trunk@311 22698fe0-d021-0410-a9a5-dc0f9d26e629

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -43,8 +43,8 @@ Spinner = {
 var Rf = {} 
 
 Rf = {
-    resetform: function() {
+    resetform: function(name) {
         $('invite_to').value = 'myfriend@mixopotamus.com';
-        $('invite_message').value = "Hi friend,\n\nHave you ever received a great mix from a friend? How about a total stranger? You can get both if you join me at Mixopotamus, a simple mix swapping project. You'll exchange an original mix CD with 5 other people, selected at random.\n\nWhat will your mix say about you? And to whom? Who knows. But I know you've got great taste in music--why not share it with some other people and discover some great new stuff while you're at it?\n\nHappy mixing!";
+        $('invite_message').value = "Hi friend,\n\nHave you ever received a great mix from a friend? How about a total stranger? You can get both if you join me at Mixopotamus, a simple mix swapping project. You'll exchange an original mix CD with 5 other people, selected at random.\n\nWhat will your mix say about you? And to whom? Who knows. But I know you've got great taste in music--why not share it with some other people and discover some great new stuff while you're at it?\n\nHappy mixing!\n" + name;
     }
 }
